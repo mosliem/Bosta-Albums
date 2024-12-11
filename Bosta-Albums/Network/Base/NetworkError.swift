242d5e96910c3528eb11a377e3ed2c6a69b5e2a7
Sliem <mohamedhu99@gmail.com>
2024-12-12 01:37:58 +0200

    Network: Create Network Error enum

--- Bosta-Albums/Network/Base/NetworkError.swift
+++ Bosta-Albums/Network/Base/NetworkError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case requestFailed(Error)
+    case invalidStatusCode(Int)
+    case decodingFailed(Error)
+    case somethingWentWrong
+
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed(let error):
+            return "Request failed with error: \(error.localizedDescription)"
+        case .invalidStatusCode(let statusCode):
+            return "Invalid status code: \(statusCode)"
+        case .decodingFailed(let error):
+            return "Failed to decode response: \(error.localizedDescription)"
+        case .somethingWentWrong:
+            return "Something went wrong. Please try again later."
+        }
+    }
+}
