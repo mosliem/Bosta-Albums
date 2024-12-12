de7e6ddb0ba85b71f8a09181c3828ba15ff52cc3
Sliem <mohamedhu99@gmail.com>
2024-12-12 18:38:54 +0200

    BaseViewModel: Create base ViewModel class and its Protocol

--- Bosta-Albums/Utils/BaseViewModel/BaseViewModelProtocol.swift
+++ Bosta-Albums/Utils/BaseViewModel/BaseViewModelProtocol.swift
@@ -5,4 +5,10 @@
 //  Created by mohamed sliem on 12/12/2024.
 //
 
-import Foundation
+import Combine
+
+protocol BaseViewModelProtocol {
+    var errorMessage: CurrentValueSubject<String, Never> { get set }
+    
+    var isLoading: CurrentValueSubject<Bool, Never>  { get }
+}
