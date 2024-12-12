d9c40e720a1c6a1cafcc283204eeee9d52e394d6
Sliem <mohamedhu99@gmail.com>
2024-12-12 17:20:40 +0200

    UIViewController+LoaderView: Extension for show and hide loader view

--- Bosta-Albums/Utils/Extensions/UIViewController+LoaderView.swift
+++ Bosta-Albums/Utils/Extensions/UIViewController+LoaderView.swift
@@ -5,4 +5,41 @@
 //  Created by mohamed sliem on 12/12/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    var loader: LoaderView {
+        
+        let window = SceneDelegate.keyWindow ?? UIWindow()
+        for view in window.subviews where view is LoaderView {
+            return view as! LoaderView
+        }
+        for view in self.view.subviews where view is LoaderView {
+            return view as! LoaderView
+        }
+        return LoaderView.instanceFromNib()
+    }
+    
+    func showAppLoader(
+        overContext: Bool = false,
+        onView view: UIView? = nil,
+        showAsBlankView: Bool = false
+    ) {
+        let loader = self.loader
+        loader.activityIndicator.color = .darkGray
+        
+        loader.frame = self.view.frame
+        loader.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(loader)
+        loader.show()
+
+    }
+    
+    func hideAppLoader() {
+        loader.hide()
+        self.loader.removeFromSuperview()
+    }
+    
+    
+}
