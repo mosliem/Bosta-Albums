62f3964694fe04bb3486330fdbbb79b6ac98f30a
Sliem <mohamedhu99@gmail.com>
2024-12-13 03:14:56 +0200

    UserProfileVM: create scene view model with protocol

--- Bosta-Albums/Scene/UserProfile/ViewModel/UserProfileVMProtocol.swift
+++ Bosta-Albums/Scene/UserProfile/ViewModel/UserProfileVMProtocol.swift
@@ -5,4 +5,15 @@
 //  Created by mohamed sliem on 12/12/2024.
 //
 
-import Foundation
+import Combine
+
+protocol UserProfileVMProtocol: BaseViewModelProtocol<UserServiceProvider> {
+    
+    var userInfo: PassthroughSubject<UserModel, Never> { get set }
+        
+    var updateAlbums: PassthroughSubject<Bool, Never> { get set }
+    
+    func viewDidLoad()
+    
+    func didPressAlbum(at index: Int)
+}
--- Bosta-Albums/Scene/UserProfile/ViewModel/UserProfileViewModel.swift
+++ Bosta-Albums/Scene/UserProfile/ViewModel/UserProfileViewModel.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import Combine
+
+class UserProfileViewModel: BaseViewModel<UserServiceProvider>, UserProfileVMProtocol {
+        
+    var userInfo = PassthroughSubject<UserModel, Never>()
+    
+    var updateAlbums = PassthroughSubject<Bool, Never>()
+    
+    var dataSourceInjection: (() -> Void)?
+    
+    private var albums: [AlbumModel] = []
+    
+    private var router: UserProfileRouterProtocol
+    
+    
+    private var userId: Int = -1 {
+        didSet {
+            getAlbums()
+        }
+    }
+    
+    
+    init(
+        router: UserProfileRouterProtocol,
+        provider: UserServiceProvider
+    ) {
+        self.router = router
+        super.init(provider: provider)
+    }
+    
+    func viewDidLoad() {
+
+        if let dataSourceInjection {
+            dataSourceInjection()
+        }
+        getUser()
+    }
+    
+    func didPressAlbum(at index: Int) {
+        router.navigationToAlbumsList(with: index)
+    }
+    
+    
+    private func getUser() {
+        provider.getUser { [weak self] result in
+            switch result {
+            case .success(let user):
+                if let user = user {
+                    self?.userInfo.send(user)
+                }
+                self?.userId = user?.id ?? 0
+            case .failure(let error):
+                self?.errorMessage.send(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func getAlbums() {
+        guard userId != -1 else {
+            return
+        }
+        
+        provider.getAlbums(userID: userId) { [weak self] result in
+            switch result {
+                
+            case .success(let albums):
+                guard let albums else {
+                    return
+                }
+                
+                self?.albums = albums
+                self?.updateAlbums.send(true)
+
+            case .failure(let error):
+                self?.errorMessage.send(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UserProfileViewModel: AlbumsDataSourceDelegate {
+    func numberOfRows() -> Int {
+        return albums.count
+    }
+    
+    func getCellTitle(index: Int) -> String {
+        return albums[index].title
+    }
+}
