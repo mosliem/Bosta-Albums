a749f225d00a7930c58a33635a1aa947a00bb09c
Sliem <mohamedhu99@gmail.com>
2024-12-13 18:07:42 +0200

    App: create app root manager

--- Bosta-Albums/Application/App.swift
+++ Bosta-Albums/Application/App.swift
@@ -1,8 +1,44 @@
 //
-//  APP.swift
+//  App.swift
 //  Bosta-Albums
 //
 //  Created by mohamed sliem on 12/12/2024.
 //
 
-import Foundation
+import UIKit
+
+class App {
+    
+    let window: UIWindow
+    var navigationController: UINavigationController
+    
+    // MARK: - Init....
+    public init(window: UIWindow) {
+        self.window = window
+        self.navigationController = UINavigationController()
+    }
+
+    func start() {
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        setupDIContainer()
+        startSceneFlow()
+    }
+    
+    private func setupDIContainer() {
+        DIContainer.shared.setDependanciesFactory(
+            viewModel: AppViewModelFactory(),
+            viewController: AppViewControllerFactory(),
+            router: AppRouterFactory(),
+            provider: AppProviderFactory()
+        )
+    }
+    
+    private func startSceneFlow() {
+        let userProfileVC = DIContainer.shared.UserProfileDI(
+            with: navigationController
+        )
+        navigationController.setViewControllers([userProfileVC], animated: true)
+    }
+}
