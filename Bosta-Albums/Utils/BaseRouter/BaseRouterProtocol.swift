34571c357117b057dee679eaad6b325acb3d4bd8
Sliem <mohamedhu99@gmail.com>
2024-12-13 03:13:22 +0200

    create base router protocol

--- Bosta-Albums/Utils/BaseRouter/BaseRouterProtocol.swift
+++ Bosta-Albums/Utils/BaseRouter/BaseRouterProtocol.swift
@@ -5,4 +5,24 @@
 //  Created by mohamed sliem on 13/12/2024.
 //
 
-import Foundation
+import UIKit
+
+protocol BaseRouterProtocol {
+    var navigationController: UINavigationController { get set }
+}
+
+extension BaseRouterProtocol {
+    func pop(animated: Bool = true) {
+        navigationController.popViewController(animated: animated)
+    }
+    
+    func push(
+        _ viewController: UIViewController,
+        animated: Bool = true
+    ) {
+        navigationController.pushViewController(
+            viewController,
+            animated: animated
+        )
+    }
+}
