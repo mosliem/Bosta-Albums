d16a1e3d5a1be3bb5ee7de01faaccfdf90148576
Sliem <mohamedhu99@gmail.com>
2024-12-15 01:45:02 +0200

    DesignSystem: Add Images enum

--- Bosta-Albums/Utils/DesignSystem/Images.swift
+++ Bosta-Albums/Utils/DesignSystem/Images.swift
@@ -5,4 +5,17 @@
 //  Created by mohamed sliem on 13/12/2024.
 //
 
-import Foundation
+import UIKit
+
+enum Images: String {
+    case downloadError = "download_error"
+}
+
+extension Images {
+    var image: UIImage? {
+        switch self {
+        case .downloadError:
+            return UIImage(named: self.rawValue)
+        }
+    }
+}
